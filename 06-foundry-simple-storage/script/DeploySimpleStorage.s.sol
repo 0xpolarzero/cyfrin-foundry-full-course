423ae9bbd249abc6690f99dc2eec5057dd2e5439
polarzero <0xpolarzero@gmail.com>
2023-09-26 15:47:01 +0200

    Add basics

---
+++ 06-foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+// Ignore the warning about source not being found.
+import {Script} from "forge-std/Script.sol";
+
+contract DeploySimpleStorage is Script {}
