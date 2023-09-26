208f86718d8be8f65315c1c51473c8f98498904e
polarzero <0xpolarzero@gmail.com>
2023-09-26 16:42:51 +0200

    chore: forge init

--- 06-foundry-simple-storage/script/DeploySimpleStorage.s.sol
+++ 06-foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-// Ignore the warning about source not being found.
-import {Script} from "forge-std/Script.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script {}
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage simpleStorage) {
+        // Won't be sent; computed before
+        // ...
+        vm.startBroadcast();
+        // Will be computed/sent along with the transaction
+
+        simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+    }
+}
+
+// 0x34A1D3fff3958843C43aD80F30b94c510645C316
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cyfrin-foundry-full-course
+
 Everything related to my progress through the Foundry full course by Patrick Collins.
